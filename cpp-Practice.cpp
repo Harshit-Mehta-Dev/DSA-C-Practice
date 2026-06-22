f11185f5ce6be0f1fe5747030b2e3bf9fc61e2d7
harshit <harshitmehta1012@gmail.com>
2026-06-22 18:45:59 +0530

    Add general C++ practice code

---
+++ cpp-Practice.cpp
@@ -0,0 +1,206 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+
+/*#include <iostream>
+using namespace std;
+int main(){
+	for(int i =0;i<=10;i++){
+		cout<<"Hello";
+	}
+	return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main(){
+	for(int i=0;i<=10;--i){
+		cout<<"Hello";
+	}
+	return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main(){
+	int i = 1;
+	while(i<=5){
+		cout<<i<<endl;
+		i++;
+	}
+	return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main(){
+	for(int i=0;i<=10;i++){
+		if(i==5){
+		break;
+		}
+		cout<< i<<" ";
+	}
+	return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main(){
+	for(int i=0;i<=10;i++){
+		if(i==5){
+		continue;
+		}
+		cout<< i<<" ";
+	}
+	return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main() {                             //nested-loop
+    for(int i = 1; i <= 4; i++) {       // Rows
+        for(int j = 1; j <= 4; j++) {  // Columns              
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main() {
+    for(int i = 1; i <= 4; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main(){
+    int n=5;                             
+    for(int row=1;row<=n;row++){       
+        for(int col=1;col<=n-row+1;col++){               
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+
+
+/*#include <iostream>
+using namespace std;
+int main(){
+  int marks[5] = {10, 20, 30, 40, 50};
+  marks[0] = 90;
+  cout<<marks[0];
+  return 0;
+}*/
+
+/*#include <iostream>
+using namespace std;
+int main(){
+  //method 1
+  int marks[5];
+  marks[0]=90;
+  marks[1]=85;
+  marks[2]=87;
+  marks[3]=92;
+  marks[4]=95;
+  //method 2
+  int group[5]={10,20,30,40,50} ;
+  //method 3
+  int data[5]={10,20};
+  return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
